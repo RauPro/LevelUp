94f9d152aefe9cb9922eab4b7ea43c5341690fe0
RauPro <00012119@uca.edu.sv>
2025-06-21 15:12:12 +0700

    Add CORS middleware and enhance problem generation response handling

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.routes import router as api_router
 
@@ -10,6 +11,13 @@ app = FastAPI(
     version="0.1.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # or ["*"] for all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root() -> dict:
